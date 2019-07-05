8adebc5beda98ac5b616f3796fc29ba688a05813
brookton <davidbrookton@gmail.com>
2019-07-05 14:34:34 -0600

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -8,12 +8,56 @@ export default class Cell extends Component {
       color: this.props.color
     }
   }
+
+  handleClick = () => {
+    // debugger
+
+    const newColor = this.props.getSelectedColor()
+    // debugger
+    console.log(this.state.color)
+    console.log(newColor)
+    // console.log(this.props.getSelectedColor)
+    this.setState({
+      color: newColor
+    })
+    // debugger
+  }
   
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      <div className="cell" onClick={this.handleClick} style={{backgroundColor: this.state.color}}>
       </div>
     )
   }
   
 }
+
+
+// import React, { Component } from 'react';
+
+// export default class Cell extends Component {
+  
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       color: this.props.color
+//     }
+//   }
+  
+//   handleClick = () => {
+//     const newColor = this.props.getSelectedColor()
+//     this.setState({
+//       color: newColor
+//     })
+//   }
+  
+//   render() {
+//     return (
+//       <div className="cell"
+//            style={{backgroundColor: this.state.color}}
+//            onClick={this.handleClick}>
+//       </div>
+//     )
+//   }
+  
+// }
